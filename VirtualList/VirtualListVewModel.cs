25b2eb9fdf0c5c5258262dd27d7023c0382ebe54
Roland Pheasant <roland@dynamic-data.org>
2016-07-13 20:19:15 +0100

    Fix virtualising list problem

--- VirtualList/VirtualListVewModel.cs
+++ VirtualList/VirtualListVewModel.cs
@@ -8,6 +8,7 @@ using DynamicDataEx;
 using System.Reactive.Disposables;
 using System.Diagnostics;
 using DynamicData.Binding;
+using DynamicData.Aggregation;//this using is importantant, otherwise it uses standard observable.Count()
 
 namespace VirtualList
 {
@@ -32,64 +33,26 @@ namespace VirtualList
             _filterController = new FilterController<Poco>();
             Items = new BindingList<Poco>();
 
-
-            /// this does not update Count, if no virtualisation request is issued
             var sharedDataSource = _service
-                                   .DataStream
-                                   .Do(x => Trace.WriteLine($"Service -> {x}"))
-                                   .ToObservableChangeSet()
-                                   .Virtualise(_virtualisingController)
-                                   .Publish();
+                .DataStream
+                .Do(x => Trace.WriteLine($"Service -> {x}"))
+                .ToObservableChangeSet()
+                .Publish();
 
             var binding = sharedDataSource
+                          .Virtualise(_virtualisingController)
                           .ObserveOn(_bindingContext)
                           .Bind(Items)
                           .Subscribe();
-
-
-            /// this works, but items are reversed in the BindingList
-            //var sharedDataSource = _service
-            //                       .DataStream
-            //                       .Do(x => Trace.WriteLine($"Service -> {x}"))
-            //                       .ToObservableChangeSet()
-            //                       .Reverse()
-            //                       .Virtualise(_virtualisingController)
-            //                       .Publish();
-
-            //var binding = sharedDataSource
-            //              .ObserveOn(_bindingContext)
-            //              .Bind(Items)
-            //              .Subscribe();
-
-
-            /// this works, but requires  transitioning from keyed Changeset then back to un-keyed ChangeSet
-            /// it also requires to sort before virtualise
-            //var sharedDataSource = _service
-            //                       .DataStream
-            //                       .Do(x => Trace.WriteLine($"Service -> {x}"))
-            //                       .ToObservableChangeSet()
-            //                       .AddKey(x => x.Id)
-            //                       .Sort(SortExpressionComparer<Poco>.Ascending(x => x.Id))
-            //                       .Virtualise(_virtualisingController)
-            //                       .Publish();
-
-            //var binding = sharedDataSource
-            //              .RemoveKey()
-            //              .ObserveOn(_bindingContext)
-            //              .Bind(Items)
-            //              .Subscribe();
-
-
-            Count = sharedDataSource.Select(x => {
-                                            return x.Response.TotalSize; });
+            
+            //the problem was because Virtualise should fire a noticiation if count changes, but it does not [BUG]
+            //Therefore take the total count from the underlying data NB: Count is DD.Count() not Observable.Count()
+            Count = sharedDataSource.Count().DistinctUntilChanged();
 
             Count.Subscribe(x => Trace.WriteLine($"Count = {x}"));
 
             var connection = sharedDataSource.Connect();
             _disposables = new CompositeDisposable(binding, connection);
-
-            //////////
-    
         }
 
 
